ad8e6d07c8cb558690a992abf940a01bb32c6477
Naveen Gupta <naveeng3074@gmail.com>
2021-11-30 15:25:29 +0530

    test
    test

---
+++ s3redshift/uploadS3FileToRedshift.js
@@ -0,0 +1,35 @@
+//https://stackoverflow.com/questions/18465313/java-sdk-for-copying-to-redshift
+//https://github.com/abhilashahyd/nodeaws/blob/main/index.js
+
+var stringify = require('csv-stringify');
+var aws = require('aws-sdk');
+var Redshift = require('node-redshift');
+
+
+var client = {
+    user: 'awsuser',
+    database: 'dev',
+    password: 'Awsredshiftpassword1',
+    port: '5439',
+    host: 'redshift-cluster-1.ctqtd45s8xvi.us-east-1.redshift.amazonaws.com',
+};
+
+var redshiftClient = new Redshift(client);
+
+var pg_query = "copy mytest from 's3://athenaddbquerydata/patient.json' " + "ACCESS_KEY_ID 'AKIA4UI4OOK4OFFTSB5E' SECRET_ACCESS_KEY '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' FORMAT JSON 'noshred';";
+
+redshiftClient.query(pg_query, { raw: true }, function(err1, data) {
+    //query completed, we can close the connection
+    // release();
+    if (err1) {
+        console.log('error here');
+        console.error(err1);
+    } else {
+        //upload successful
+        console.log('upload success');
+        console.log(data);
+
+    }
+});
+//  redshiftClient.close(); 
+console.log('outside');
